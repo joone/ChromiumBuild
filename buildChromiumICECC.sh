74a1cd743f14d091b70a9b2bd93fbf06fa1a5dea
Joone Hur <joone.hur@intel.com>
2018-09-27 21:33:54 +0000

    ozone-gbm support

--- buildChromiumICECC.sh
+++ buildChromiumICECC.sh
@@ -8,13 +8,13 @@ then
 fi
 
 export CCACHE_PREFIX=icecc
-export CCACHE_BASEDIR=$HOME/git/cr-wl
+export CCACHE_BASEDIR=$HOME/git/chromium/
 export CHROME_DEVEL_SANDBOX=/usr/local/sbin/chrome-devel-sandbox
 export ICECC_CLANG_REMOTE_CPP=1
 
 # Please set your path to ICECC_VERSION and CHROMIUM_SRC.
-export ICECC_VERSION=$HOME/git/cr-wl/clang.tar.gz
-export CHROMIUM_SRC=$HOME/git/cr-wl/src
+export ICECC_VERSION=$HOME/git/chromium/clang.tar.gz
+export CHROMIUM_SRC=$HOME/git/chromium/src
 
 export PATH=/usr/lib/ccache:/usr/lib/icecc/bin:$PATH
 export PATH=$CHROMIUM_SRC/third_party/llvm-build/Release+Asserts/bin:$PATH
@@ -46,10 +46,10 @@ then
 fi
 
 # Set Chromium gn build arguments.
-export GN_DEFINES='is_component_build=true'
+export GN_DEFINES='is_component_build=false'
 export GN_DEFINES=$GN_DEFINES' enable_nacl=false treat_warnings_as_errors=false'
 export GN_DEFINES=$GN_DEFINES' proprietary_codecs=true ffmpeg_branding="Chrome"'
-export GN_DEFINES=$GN_DEFINES' linux_use_bundled_binutils=false clang_use_chrome_plugins=false cc_wrapper="ccache" ffmpeg_use_atomics_fallback=true enable_swiftshader=false use_jumbo_build = true '
+export GN_DEFINES=$GN_DEFINES' linux_use_bundled_binutils=false clang_use_chrome_plugins=false cc_wrapper="ccache" ffmpeg_use_atomics_fallback=true enable_swiftshader=false use_jumbo_build = false '
 export GN_DEFINES=$GN_DEFINES' google_api_key="???" google_default_client_id="??.com" google_default_client_secret="??"'
 timestamp=$(date +"%T")
 echo "[$timestamp] 1. Configuration"
@@ -57,13 +57,13 @@ echo "[$timestamp] 1. Configuration"
 # Start building Chromium using the gn configuration.
 if [ "$1" == Debug ];
 then
-  export GN_DEFINES=$GN_DEFINES' use_ozone=true enable_mus=true use_xkbcommon=true'
-  export GN_DEFINES=$GN_DEFINES' dcheck_always_on=true'
+ # export GN_DEFINES=$GN_DEFINES' use_ozone=true enable_mus=true use_xkbcommon=true'
+  export GN_DEFINES=$GN_DEFINES' dcheck_always_on=false'
   echo "GN_DEFINES: "$GN_DEFINES
   gn gen out/Debug "--args=is_debug=true $GN_DEFINES"
 elif [ "$1" == Release ];
 then
-  export GN_DEFINES=$GN_DEFINES' use_ozone=true enable_mus=true use_xkbcommon=true'
+  #export GN_DEFINES=$GN_DEFINES' use_ozone=true enable_mus=true use_xkbcommon=true'
   echo "GN_DEFINES: "$GN_DEFINES
   gn gen out/Release "--args=is_debug=false $GN_DEFINES"
 elif [ "$1" == vulkan ];
@@ -73,18 +73,42 @@ then
   gn gen out/vulkan "--args=is_debug=true $GN_DEFINES"
 elif [ "$1" == gbm_cros ];
 then
-  export GN_DEFINES=$GN_DEFINES' target_os="chromeos" internal_window_mode=true'
-  echo "GN_DEFINES: "$GN_DEFINES
-  gn gen out/gbm_cros "--args=is_debug=false $GN_DEFINES"
-elif [ "$1" == gbm_rel ];
-then
-  export GN_DEFINES=$GN_DEFINES' clang_use_chrome_plugins=false cc_wrapper="ccache"'
-  export GN_DEFINES=$GN_DEFINES' use_ozone=true ozone_platform_gbm=true ozone_platform_x11=false ozone_platform_wayland=false ozone_auto_platforms=false use_cups=false'
-  export GN_DEFINES=$GN_DEFINES' use_gnome_keyring=false use_pulseaudio=false use_sysroot=true'
-  export GN_DEFINES=$GN_DEFINES' use_gio=false remove_webcore_debug_symbols=true enable_native_notifications=false'
+  export GN_DEFINES=$GN_DEFINES' target_os="chromeos" use_ozone=true'
+  export GN_DEFINES=$GN_DEFINES' ozone_platform_gbm=true ozone_platform_x11=false ozone_platform_wayland=false ozone_auto_platforms=false use_cups=false'
+  export GN_DEFINES=$GN_DEFINES' cc_wrapper="ccache"'
   export GN_DEFINES=$GN_DEFINES' use_intel_minigbm = true'
   echo "GN_DEFINES: "$GN_DEFINES
-  DRV_I915=1 gn gen out/gbm_rel "--args=$GN_DEFINES"
+  gn gen out/gbm_cros "--args=is_debug=true $GN_DEFINES"
+elif [ "$1" == gbm_rel_cros ];
+then
+  export GN_DEFINES=$GN_DEFINES' target_os="chromeos" use_ozone=true'
+  export GN_DEFINES=$GN_DEFINES' ozone_platform_gbm=true ozone_platform_x11=false ozone_platform_wayland=false ozone_auto_platforms=false use_cups=false'
+  export GN_DEFINES=$GN_DEFINES' cc_wrapper="ccache"'
+  export GN_DEFINES=$GN_DEFINES' use_intel_minigbm = true'
+  export GN_DEFINES=$GN_DEFINES' toolkit_views = true'
+  echo "GN_DEFINES: "$GN_DEFINES
+  gn gen out/gbm_rel_cros "--args=is_debug=false $GN_DEFINES"
+elif [ "$1" == gbm_rel ];
+then
+  export GN_DEFINES=$GN_DEFINES' use_ozone=true ozone_platform_gbm=true'
+  export GN_DEFINES=$GN_DEFINES' remove_webcore_debug_symbols=true'
+  export GN_DEFINES=$GN_DEFINES' cc_wrapper="ccache"'
+  export GN_DEFINES=$GN_DEFINES' use_intel_minigbm = true'
+  export GN_DEFINES=$GN_DEFINES' build_display_configuration = true'
+  export GN_DEFINES=$GN_DEFINES' toolkit_views = false use_vaapi = true' 
+  export GN_DEFINES=$GN_DEFINES' is_debug=false' 
+  echo "GN_DEFINES: "$GN_DEFINES
+  gn gen out/gbm_rel "--args=$GN_DEFINES"
+elif [ "$1" == gbm_debug ];
+then
+  export GN_DEFINES=$GN_DEFINES' use_ozone=true ozone_platform_gbm=true'
+  export GN_DEFINES=$GN_DEFINES' remove_webcore_debug_symbols=true'
+  export GN_DEFINES=$GN_DEFINES' cc_wrapper="ccache"'
+  export GN_DEFINES=$GN_DEFINES' use_intel_minigbm = true'
+  export GN_DEFINES=$GN_DEFINES' build_display_configuration = true'
+  export GN_DEFINES=$GN_DEFINES' toolkit_views = false use_vaapi = true' 
+  echo "GN_DEFINES: "$GN_DEFINES
+  gn gen out/gbm_debug "--args=$GN_DEFINES"
 else
   echo "Undefined Debug or Release."
   exit 0
@@ -93,7 +117,7 @@ echo ""
 
 start_timestamp=$(date +"%T")
 echo "[$start_timestamp] 2. Start compiling Chromium on $1 mode"
-ninja -j 100 -C out/"$1" $2
+ninja -j 150 -C out/"$1" $2
 end_timestamp=$(date +"%T")
 echo ""
 echo "[$end_timestamp] 3. Finish to compile Chromium."
